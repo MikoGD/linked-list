fed4c99c68abbb45e55dcc1a109961513c65c189
Mikael Escolin <mikaelescolin@gmail.com>
2019-10-12 22:46:57 +0100

    Added insert_node function

--- linked-list.c
+++ linked-list.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct AlphaNode
 {
@@ -19,7 +20,54 @@ void print_node(Alpha *, char data);
 
 int main()
 {
+    // Where the list starts
+    // The head will point to the first node of the list when the first node
+    //  exits
+    Alpha *head = NULL;
     printf("Hello World");
 
     return 0;
 }//END main()
+
+void insert_node(Alpha **head_ptr, char data)
+{ 
+    // Create new node
+    Alpha *new_node = (Alpha *) malloc(sizeof(Alpha));
+    
+    // Checks if memory was allcoated
+    // If yes assign the member's values
+    if (new_node != NULL)
+    {
+        new_node->data = data;
+        new_node->next_node = NULL;
+    }
+    else
+    {
+        printf("ERROR: Not enough memory to create a new node\n\n");
+        return void;
+    }
+    
+    /* If the list is empty
+     *  Add the new node in at the beginning of the list.
+     * Else create a pointer previous_node
+     *  and go through the list until the current node it is on data is greater than the
+     *  new node's data.
+     * Insert the node before the current node.
+     */
+
+    if (*head_ptr == NULL)
+    {
+        *head_ptr->next_node;
+        next_node->NULL;
+    }
+    else
+    {
+        Alpha *previous_node = NULL;
+
+        previous_node = search_list(head_ptr, data);
+        new_node->next_node = previous_node->next_node;
+        previous_node->next_node = new_node;
+    }//END IF
+
+    return void;
+}//END insert_node()
